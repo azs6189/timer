3fa90a05ab459f88ba2383eea7e88386969a8520
Allie Soliz <solizale@gmail.com>
2022-08-15 20:01:54 -0400

    event classes added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi there!");
+class Timer {
+	constructor(durationInput, startButton, pauseButton) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+		this.startButton.addEventListener("click", this.start);
+	}
+	start() {
+		console.log("Time to start the timer!");
+	}
+}
+
+new Timer("hi there");
