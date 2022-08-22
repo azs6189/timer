fec77aad5cfabe18fbca54253790ea935d9c3f6b
Allie Soliz <solizale@gmail.com>
2022-08-22 00:51:32 -0400

    first pass on animation

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const circle = document.querySelector("circle");
 const perimeter = circle.getAttribute("r") * 2 * Math.PI;
 circle.setAttribute("stroke-dasharray", perimeter);
 
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
 	onStart() {
 		console.log("Timer Started");
 	},
-
 	onTick() {
-		console.log("Timer just ticked down");
+		circle.setAttribute("stroke-dashoffset", currentOffset);
+		currentOffset = currentOffset - 50;
 	},
-
 	onComplete() {
 		console.log("Timer is completed");
 	},
