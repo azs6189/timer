1be5059991e2ecb6da957cadf233566fa2021932
Allie Soliz <solizale@gmail.com>
2022-08-22 02:59:58 -0400

    updated parameter in Timer methods

--- timer.js
+++ timer.js
@@ -19,7 +19,7 @@ class Timer {
 			this.onStart(this.timeRemaining);
 		}
 		this.tick();
-		this.interval = setInterval(this.tick, 50);
+		this.interval = setInterval(this.tick, 20);
 	};
 
 	pause = () => {
@@ -33,9 +33,9 @@ class Timer {
 				this.onComplete();
 			}
 		} else {
-			this.timeRemaining = this.timeRemaining - 0.05;
+			this.timeRemaining = this.timeRemaining - 0.02;
 			if (this.onTick) {
-				this.onTick();
+				this.onTick(this.timeRemaining);
 			}
 		}
 	};
